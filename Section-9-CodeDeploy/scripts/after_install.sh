2cf7205e605beb1be9ce7364be63eff7b9bd3257
Nachiketh <murthy@manifoldailearning.in>
2023-06-14 22:36:13 +0800

    Deploy code

--- Section-9-CodeDeploy/scripts/after_install.sh
+++ Section-9-CodeDeploy/scripts/after_install.sh
@@ -2,5 +2,5 @@
 EC2_INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 EC2_INSTANCE_TYPE=$(curl -s http://169.254.169.254/latest/meta-data/instance-type)
 EC2_AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
-sed -i "s/you are on Track/This is currently runnining on $EC2_INSANCE_TYPE with id - $EC2_INSTANCE_ID on AM - $EC2_AZ/g" /var/www/html/index.html
+sed -i "s/you are on Track/This is currently runnining on $EC2_INSANCE_TYPE with id - $EC2_INSTANCE_ID on AZ - $EC2_AZ/g" /var/www/html/index.html
 chmod 664 /var/www/html/index.html
